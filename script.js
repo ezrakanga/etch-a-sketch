5620259291f4262f64e97ff20c9d0f7324426fcc
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-03-18 13:09:57 +0300

    Created 4 divs are examples before adding the rest

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container');
+
+const one = document.createElement('div');
+one.setAttribute('class', 'square');
+one.setAttribute('id', 'one');
+one.style.width = '200px';
+one.style.height = '200px';
+container.appendChild(one);
+
+const two = document.createElement('div');
+two.setAttribute('class', 'square');
+two.setAttribute('id', 'two');
+two.style.width = '200px';
+two.style.height = '200px';
+container.appendChild(two);
+
+const three = document.createElement('div');
+three.setAttribute('class', 'square');
+three.setAttribute('id', 'three');
+three.style.width = '200px';
+three.style.height = '200px';
+container.appendChild(three);
+
+const four = document.createElement('div');
+four.setAttribute('class', 'square');
+four.setAttribute('id', 'four');
+four.style.width = '200px';
+four.style.height = '200px';
+container.appendChild(four);
