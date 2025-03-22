e25d0b164c5eabee581f4ec40c2d91a6be023b59
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-03-22 13:55:11 +0300

    Created the 16*16 grid

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
 const container = document.querySelector('.container');
 
-const one = document.createElement('div');
-one.setAttribute('class', 'square');
-one.setAttribute('id', 'one');
-one.style.width = '200px';
-one.style.height = '200px';
-container.appendChild(one);
+for (let i = 0; i < 256; i++) {
 
-const two = document.createElement('div');
-two.setAttribute('class', 'square');
-two.setAttribute('id', 'two');
-two.style.width = '200px';
-two.style.height = '200px';
-container.appendChild(two);
-
-const three = document.createElement('div');
-three.setAttribute('class', 'square');
-three.setAttribute('id', 'three');
-three.style.width = '200px';
-three.style.height = '200px';
-container.appendChild(three);
-
-const four = document.createElement('div');
-four.setAttribute('class', 'square');
-four.setAttribute('id', 'four');
-four.style.width = '200px';
-four.style.height = '200px';
-container.appendChild(four);
+  const square = document.createElement('div');
+  square.setAttribute('class', 'square');
+  container.appendChild(square);
+}
