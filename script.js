27a770e92b3d1f902befd64d4732ddf1bc906dcc
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-03-24 10:39:07 +0300

    Put an event delegation each square is targeted on hover with 'mouseover'

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector('.container');
 
+// create 16*16 grid
 for (let i = 0; i < 256; i++) {
 
   const square = document.createElement('div');
   square.setAttribute('class', 'square');
   container.appendChild(square);
 }
+
+// event delegation of hover effect
+container.addEventListener('mouseover', (e) => {
+  if (e.target.classList.contains('square')) {
+    e.target.style.backgroundColor = 'bisque';
+  }
+})
