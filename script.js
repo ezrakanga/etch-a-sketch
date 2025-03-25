3309fa40cd9d14f842e34aafc19f9497b375e8b2
Ezra <191688309+ezrakanga@users.noreply.github.com>
2025-03-25 09:41:25 +0300

    Changed whole code removed the fixed 16*16 grid added button that made grid flexible to changes added button that resets the grid to initial color

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
+const body = document.querySelector('body');
 const container = document.querySelector('.container');
 
-// create 16*16 grid
-for (let i = 0; i < 256; i++) {
+// grid creation
+function createGrid(squarePerSide) {
 
-  const square = document.createElement('div');
-  square.setAttribute('class', 'square');
-  container.appendChild(square);
+  // clear existing grid
+  container.innerHTML = '';
+
+  // calculate cell dimensions
+  let percentageSquareSize = 100 / squarePerSide;
+
+  // create squares
+  for (let i = 0; i < squarePerSide * squarePerSide; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.flex = `0 0 ${percentageSquareSize}%`;
+    square.style.height = `${percentageSquareSize}%`;
+    container.appendChild(square);
+  }  
+}
+createGrid(8);
+
+// grid resizing
+function changeGridSize() {
+  let newSize = prompt('How many squares do you want (max 100)?', 8);
+  newSize = parseInt(newSize);
+
+  if (newSize > 0 && newSize <= 100) {
+    createGrid(newSize);
+  } else {
+    alert('Choose number between 1-100!');
+    changeGridSize();
+  }
 }
 
-// event delegation of hover effect
+// hover effect
 container.addEventListener('mouseover', (e) => {
   if (e.target.classList.contains('square')) {
     e.target.style.backgroundColor = 'bisque';
   }
+})
+
+// button creation
+const btn = document.createElement('button');
+btn.classList.add('btn');
+btn.textContent = 'Change grid size';
+body.appendChild(btn);
+
+// button event handler
+btn.addEventListener('click', () => {
+  changeGridSize();
+})
+
+// reset button creation
+const reset = document.createElement('button');
+reset.classList.add('reset');
+reset.textContent = 'Reset';
+body.appendChild(reset);
+
+// button event handler
+reset.addEventListener('click', () => {
+  document.querySelectorAll('.square').forEach(square => {
+    square.style.backgroundColor = 'lightblue';
+  })
 })
